463c41107b81c67ce7f20a1172c70596a82907f2
Matvei Ivanov <matvivan@e1z1r8p1.42.us.org>
2020-03-04 08:51:39 -0800

    modif main

--- main.c
+++ main.c
@@ -13,7 +13,6 @@
 #include "fractol.h"
 #include <stdio.h>
 
-
 void				putpxl(t_pxl d, int x, int y, unsigned int color)
 {
 	int				i;
@@ -31,83 +30,101 @@ void				putpxl(t_pxl d, int x, int y, unsigned int color)
 		}
 }
 
-unsigned int		palette(int iter)
+unsigned int		palette(double iter)
 {
-	unsigned int color;
+	unsigned int	color;
 
 	color = 0;
-	if (iter < 1)
-		color = 0;
-	else if (iter <= 160)
-		color = (iter / 5 << 16) + ((7 + iter * 100 / 160) << 8) + (100 + iter * 103 / 160);
-	else if (iter <= 420)
+	if (iter <= 0.84)
 	{
-		iter -= 160;
-		color = ((32 + iter * 205 / 260) << 16) + ((107 + iter * 148 / 260) << 8) + (152 + iter * 103 / 260);
-	}
-	else if (iter <= 642)
-	{
-		iter -= 420;
-		color = ((237 + iter * 18 / 222) << 16) + ((255 - iter * 85 / 222) << 8) + (255 - iter * 255 / 222);
-	}
-	else if (iter <= 857)
-	{
-		iter -= 642;
-		color = ((255 - iter * 255 / 215) << 16) + ((170 - iter * 168 / 215) << 8);
-	}
-	else if (iter < 999)
-	{
-		iter -= 857;
-		color = 0x000200 + iter * 170 / 142;
+		iter /= 0.42; /* period from 0.0 to 2.0 */
+		/*		 red		*/
+		color += 237 * (1 - fabs(iter - 1));
+		if (iter <= 1)
+			color -= 140 * (0.5 - fabs(iter - 0.5));
+		else
+			color += 272 * (0.5 - fabs(iter - 1.5));
+		color *= 0x100;
+		/*		green		*/
+		color += 255 * (1 - fabs(iter - 1));
+		if (iter > 1)
+			color += 84 * (0.5 - fabs(iter - 1.5));
+		color *= 0x100;
+		/*		 blue		*/
+		color += 255 * (1 - pow(iter - 1, 2) * 0.52 * pow(6, (int)iter));
 	}
 	return (color);
 }
 
-unsigned int		mandelbrot(t_pxl d, t_cmpl z, t_cmpl c, float max)
+unsigned int		ft_fatou(t_pxl d, t_cmpl z, t_cmpl c, float max)
 {
 	int				iter;
-	double			old_re;
-	double			smooth;
+	double			sqr_re;
+	double			sqr_im;
 
 	iter = 0;
-	while (z.re * z.re + z.im * z.im < 4 && ++iter < max)
+	while ((sqr_re = z.re * z.re) + (sqr_im = z.im * z.im) < 4 && iter < max)
 	{
-		old_re = z.re;
-		z.re = z.re * z.re - z.im * z.im + c.re;
-		z.im = 2 * old_re * z.im + c.im;
+		z.im = z.im * (z.re + z.re) + c.im;
+		z.re = sqr_re - sqr_im + c.re;
+		++iter;
 	}
-	return (palette(1000 * iter / max));/*
-	smooth = log2(log2(z.re * z.re + z.im * z.im) / 2);
-	return (palette((int)(sqrt(iter + 10 - smooth) * 256) % 1024 / 1.024));*/
+	return (palette(iter / max));
 }
 
-void				fill(t_pxl data, double coef, float marg_x, float marg_y)
+void				successive(t_mlx *ptr, t_cmpl c, int chunk)
+{
+	int				i;
+	int				j;
+	t_cmpl			z;
+	unsigned int	color;
+
+	i = -1;
+	while (++i < ptr->pxl->size)
+	{
+		z.re = 4.0 * i / ptr->pxl->size - 2;
+		j = -1;
+		while (++j < ptr->pxl->size)
+		{
+			z.im = 4.0 * j / ptr->pxl->size - 2;
+			if (i % chunk == 0 || j % chunk == 0)
+				color = ft_fatou(*(ptr->pxl), z, c, 100);
+			putpxl(*(ptr->pxl), i, j, color);
+		}
+	}
+	mlx_put_image_to_window(ptr->mlx, ptr->window, ptr->image, 0, 0);
+}
+
+void				fill(t_pxl data, double coef, float marg_re, float marg_im)
 {
 	long double		i;
 	long double		j;
 	t_cmpl			c;
-	static float	max = 50;
+	static float	max = 48;
 	static double	scale = 4;
-	static double	init_x = -2;
-	static double	init_y = -2;
+	static double	init_re = -2;
+	static double	init_im = -2;
 
 	if (coef != 1)
 	{
-		init_x += scale * (marg_x - coef / 2);
-		init_y += scale * (marg_y - coef / 2);
+		init_re += scale * (marg_re - coef / 2);
+		init_im += scale * (marg_im - coef / 2);
 		scale *= coef;
 	}
-	max *= 1.08 - max * max / 1000000;
+	if (coef == 1)
+		++max;
+	else
+		max *= 1.08 - max * max / 10000000;
 	printf("%f: ", max);
 	i = 0;
 	while (i < data.size)
 	{
-		c.re = init_x + scale * i / (data.size - 1);
+		c.re = init_re + scale * i / (data.size - 1);
 		j = 0;
 		while (j < data.size)
 		{
-			c.im = init_y + scale * j / (data.size - 1);
-			putpxl(data, i, j, mandelbrot(data, c, c, max));
+			c.im = init_im + scale * j / (data.size - 1);
+			putpxl(data, i, j, ft_fatou(data, c, c, max));
 			++j;
 		}
 		++i;
@@ -115,12 +132,24 @@ void				fill(t_pxl data, double coef, float marg_x, float marg_y)
 	printf("%Lf, %Lf\n", c.re, c.im);
 }
 
+int					plane(int x, int y, t_mlx *ptr)
+{
+	t_cmpl			c;
+	int				chunk;
+
+	chunk = 2;
+	c.re = 4.0 * x / ptr->pxl->size - 2;
+	c.im = 4.0 * y / ptr->pxl->size - 2;
+	while ((chunk /= 2))
+		successive(ptr, c, chunk);
+	return (1);
+}
+
 int					key_hook(int key, t_mlx *ptr)
 {
 	if (key == 53)
 	{
 		mlx_destroy_image(ptr->mlx, ptr->image);
-//		system("leaks fractol | grep Leak");
 		exit(0);
 	}
 	else if (key == 69)
@@ -135,8 +164,20 @@ int					key_hook(int key, t_mlx *ptr)
 
 int					mouse_hook(int button, int x, int y, t_mlx *ptr)
 {
-	fill(*ptr->pxl, 0.8, x * 1. / ptr->pxl->size, y * 1. / ptr->pxl->size);
-	mlx_put_image_to_window(ptr->mlx, ptr->window, ptr->image, 0, 0);
+	t_cmpl			c;
+
+	if (button == 1)
+	{
+		fill(*ptr->pxl, 0.9, x * 1. / ptr->pxl->size, y * 1. / ptr->pxl->size);
+	//	plane(x, y, ptr);
+		mlx_put_image_to_window(ptr->mlx, ptr->window, ptr->image, 0, 0);
+	}
+	if (button == 2)
+	{
+		c.re = 4.0 * x / ptr->pxl->size - 2;
+		c.im = 4.0 * y / ptr->pxl->size - 2;
+		printf("%x\n", ft_fatou(*ptr->pxl, c, c, 50));
+	}
 	return (0);
 }
 
@@ -156,8 +197,9 @@ int					main(void)
 	fill(data, 1, 1, 1);
 	mlx_put_image_to_window(ptr.mlx, ptr.window, ptr.image, 0, 0);
 
-	mlx_key_hook(ptr.window, &key_hook, &ptr);
 	mlx_mouse_hook(ptr.window, &mouse_hook, &ptr);
+	mlx_key_hook(ptr.window, &key_hook, &ptr);
+//	mlx_hook(ptr.window, 6, 0, &plane, &ptr);
 	mlx_hook(ptr.window, 17, 0, (int (*)())&exit, NULL);
 	mlx_loop(ptr.mlx);
 
